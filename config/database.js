a30e1a03d8c9f963bfa2b1011cc96ef59343b50d
marvin-hinkley <hinkley.marvin@gmail.com>
2021-04-14 11:38:11 -0700

    basic entities working

--- config/database.js
+++ config/database.js
@@ -1,15 +1,19 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-      },
-      options: {
-        useNullAsDefault: true,
-      },
-    },
-  },
-});
+	defaultConnection: 'default',
+	connections: {
+		default: {
+			connector: 'bookshelf',
+			settings: {
+				client: 'postgres',
+				host: env('DATABASE_HOST', 'localhost'),
+				port: env.int('DATABASE_PORT', 5432),
+				database: env('DATABASE_NAME', 'goldhill_strapi'),
+				username: env('DATABASE_USERNAME', 'postgres'),
+				password: env('DATABASE_PASSWORD', 'password'),
+			},
+			options: {
+				useNullAsDefault: true,
+			},
+		},
+	},
+})
