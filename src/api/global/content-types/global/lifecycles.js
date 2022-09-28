8bdc9cd8a665e21327ab83b4864a13f01063c927
marvin.hinkley <marvin.hinkley@americaneagle.com>
2022-09-28 13:57:35 -0700

    triggering github repo dispatches

---
+++ src/api/global/content-types/global/lifecycles.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+
+module.exports = {
+  async afterCreate(event) {
+    await dispatchGithub();
+  },
+  async afterUpdate(event) {
+    await dispatchGithub();
+  },
+  async afterDelete(event) {
+    await dispatchGithub();
+  },
+};
+
+const dispatchGithub = async () => {
+  try {
+      const dispatchResult = await axios.post(
+        `${process.env.GITHUB_GATSBY_REPO_URL}/dispatches`,
+        { event_type: event },
+        {
+          Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+          Accept: 'application/vnd.github.v3+json',
+          "Content-Type": "application/json",
+        }
+      );
+
+      console.log('dispatched build trigger', dispatchResult);
+    } catch (error) {
+      console.log('problem dispatching build trigger', error);
+    }
+}
